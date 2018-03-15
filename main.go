d1efc77e8e8bf82e9eb0f9e17d60a25dac5d2c52
csuhan <1593407377@qq.com>
2018-03-15 22:17:10 +0800

    wxapp branch init

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/astaxie/beego"
 	"github.com/csuhan/csugo/controllers"
+	"github.com/csuhan/csugo/models"
 	_ "github.com/csuhan/csugo/routers"
 )
 
@@ -10,5 +11,6 @@ func main() {
 	//日志
 	beego.SetLogger("file", `{"filename":"logs/project.log","level":7,"maxlines":0,"maxsize":0,"daily":true,"maxdays":10}`)
 	beego.ErrorController(&controllers.ErrorController{})
+	models.InitDB() //初始化数据库连接
 	beego.Run()
 }
