3f12e91aab88231ebb3043677b6ad031659ea882
csuhan <1593407377@qq.com>
2018-03-15 22:00:03 +0800

    add some error type

--- utils/errors.go
+++ utils/errors.go
@@ -3,8 +3,10 @@ package utils
 import "errors"
 
 var (
-	ERROR_SERVER = errors.New("服务器出了点问题，重试一下？")
-	ERROR_ID_PWD = errors.New("账号或者密码错误,请重新输入")
-	ERROR_UNKOWN = errors.New("未知错误")
-	ERROR_DATA=errors.New("数据错误")
+	ERROR_SERVER  = errors.New("服务器出了点问题，重试一下？")
+	ERROR_ID_PWD  = errors.New("账号或者密码错误,请重新输入")
+	ERROR_UNKOWN  = errors.New("未知错误")
+	ERROR_JWC     = errors.New("教务系统出了点问题,请重试")
+	ERROR_DATA    = errors.New("数据错误")
+	ERROR_NO_USER = errors.New("用户不存在")
 )
