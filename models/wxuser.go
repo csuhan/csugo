4001b42ca04d9438dec4e70f9d0535c8465242b7
csuhan <1593407377@qq.com>
2018-03-15 21:52:03 +0800

    add wxapp login method

--- models/wxuser.go
+++ models/wxuser.go
@@ -1,42 +1,42 @@
 package models
 
 import (
-	"github.com/boltdb/bolt"
 	"encoding/json"
+	"github.com/boltdb/bolt"
 	"github.com/csuhan/csugo/utils"
 )
 
-type WxUser struct{
-	Code string  `json:"code"`
-	OpenID string `json:"openid"`
+type WxUser struct {
+	Code       string `json:"code"`
+	OpenID     string `json:"openid"`
 	SessionKey string `json:"session_key"`
-	WxToken string	`json:"wxtoken"`
-	SchoolID string `json:"schoolid"`
-	JwcPwd string	`json:"jwcpwd"`	//教务处
-	ItsPwd string 	`json:"itspwd"`	//信息门户
-	LibPwd string 	`json:"libpwd"`	//图书馆
+	WxToken    string `json:"wxtoken"`
+	SchoolID   string `json:"schoolid"`
+	JwcPwd     string `json:"jwcpwd"` //教务处
+	ItsPwd     string `json:"itspwd"` //信息门户
+	LibPwd     string `json:"libpwd"` //图书馆
 }
 
 //插入用户信息
-func (this *WxUser)Insert()error{
-	db,err:=GetDB()
-	if err!=nil{
+func (this *WxUser) Insert() error {
+	db, err := GetDB()
+	if err != nil {
 		return err
 	}
 	defer db.Close()
-	data,err:=json.Marshal(this)
-	if err!=nil{
+	data, err := json.Marshal(this)
+	if err != nil {
 		return err
 	}
 	db.Update(func(tx *bolt.Tx) error {
-		return tx.Bucket([]byte("wxuser")).Put([]byte(this.WxToken),data)
+		return tx.Bucket([]byte("wxuser")).Put([]byte(this.WxToken), data)
 	})
 	return nil
 }
 
 //获取用户信息
-func (this *WxUser)Get()error{
-	db,err:=GetDB()
+func (this *WxUser) Get() error {
+	db, err := GetDB()
 	if err != nil {
 		return utils.ERROR_SERVER
 	}
@@ -47,7 +47,7 @@ func (this *WxUser)Get()error{
 		return nil
 	})
 	//用户不存在
-	if json.Unmarshal(value,this)!=nil{
+	if json.Unmarshal(value, this) != nil {
 		return utils.ERROR_NO_USER
 	}
 	return nil
