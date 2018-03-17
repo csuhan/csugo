7dffca5480d6622860dac5b32d8d10f7e7c6cb4f
csuhan <1593407377@qq.com>
2018-03-17 14:54:54 +0800

    add library reloan mothod

--- controllers/lib.go
+++ controllers/lib.go
@@ -1,62 +1,91 @@
 package controllers
 
 import (
-	"github.com/csuhan/csugo/models"
 	"github.com/astaxie/beego"
+	"github.com/csuhan/csugo/models"
+	"strings"
 )
 
-type LibController struct{
+type LibController struct {
 	beego.Controller
 }
 
 // @router /lib/login/:id/:pwd [get]
-func (this *LibController)Login(){
-	lib:=&models.Lib{}
-	_,err:=lib.Login(this.Ctx.Input.Param(":id"),this.Ctx.Input.Param(":pwd"))
-	if err!=nil{ //登录失败
-		this.Data["json"]= struct {
+func (this *LibController) Login() {
+	lib := &models.Lib{}
+	_, err := lib.Login(this.Ctx.Input.Param(":id"), this.Ctx.Input.Param(":pwd"))
+	if err != nil { //登录失败
+		this.Data["json"] = struct {
 			StateCode int
-			Error string
+			Error     string
 		}{
-			StateCode:-1,
-			Error:err.Error(),
+			StateCode: -1,
+			Error:     err.Error(),
 		}
 		this.ServeJSON()
 	}
 	//登录成功
-	this.Data["json"]= struct {
+	this.Data["json"] = struct {
 		StateCode int
-		Error string
+		Error     string
 	}{
-		StateCode:1,
-		Error:"",
+		StateCode: 1,
+		Error:     "",
 	}
 	this.ServeJSON()
 }
 
 // @router /lib/list/:id/:pwd [get]
-func (this *LibController)List(){
-	lib:=&models.Lib{}
-	books,err:=lib.List(this.Ctx.Input.Param(":id"),this.Ctx.Input.Param(":pwd"))
-	if err!=nil{
-		this.Data["json"]= struct {
+func (this *LibController) List() {
+	lib := &models.Lib{}
+	books, err := lib.List(this.Ctx.Input.Param(":id"), this.Ctx.Input.Param(":pwd"))
+	if err != nil {
+		this.Data["json"] = struct {
 			StateCode int
-			Error string
+			Error     string
 		}{
-			StateCode:-1,
-			Error:err.Error(),
+			StateCode: -1,
+			Error:     err.Error(),
 		}
 		this.ServeJSON()
 	}
 	//登录成功
-	this.Data["json"]= struct {
+	this.Data["json"] = struct {
 		StateCode int
-		Error string
-		Books []models.Book
+		Error     string
+		Books     []models.Book
 	}{
-		StateCode:1,
-		Error:"",
-		Books:books,
+		StateCode: 1,
+		Error:     "",
+		Books:     books,
+	}
+	this.ServeJSON()
+}
+
+// @router /lib/reloan/:id/:pwd/:books [get]
+func (this *LibController) Reloan() {
+	bookBarCodes := strings.Split(this.Ctx.Input.Param(":books"), "+")
+	lib := &models.Lib{}
+	books, err := lib.Borrow(this.Ctx.Input.Param(":id"), this.Ctx.Input.Param(":pwd"), bookBarCodes)
+	if err != nil {
+		this.Data["json"] = struct {
+			StateCode int
+			Error     string
+		}{
+			StateCode: -1,
+			Error:     err.Error(),
+		}
+		this.ServeJSON()
+	}
+
+	this.Data["json"] = struct {
+		StateCode int
+		Error     string
+		Books     []models.Book
+	}{
+		StateCode: 1,
+		Error:     "",
+		Books:     books,
 	}
 	this.ServeJSON()
 }
--- models/lib.go
+++ models/lib.go
@@ -1,92 +1,93 @@
 package models
 
 import (
-	"net/http"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
 	"github.com/csuhan/csugo/utils"
+	"io/ioutil"
+	"net/http"
 	"net/http/cookiejar"
 	"net/url"
-	"github.com/PuerkitoBio/goquery"
 	"strings"
-	"io/ioutil"
 )
 
 const LIB_LOGIN_URL = "http://opac.its.csu.edu.cn/NTRdrLogin.aspx"
 const LIB_BOOK_URL = "http://opac.its.csu.edu.cn/NTBookLoanRetr.aspx"
+const LIB_REBORROW_URL = "http://opac.its.csu.edu.cn/NTBookloanResult.aspx"
 
 type Lib struct {
-
 }
 
-type Book struct{
-	BarCode,BookName,BookNo,Author,Place,BorrowedDate,ReturnedDate,Price,BorrowTimes string
+type Book struct {
+	BarCode, BookName, BookNo, Author, Place, BorrowedDate, ReturnedDate, Price, BorrowTimes, ReloanRes string
 }
 
 //登录系统
-func (this *Lib)Login(ID,Pwd string)(*http.Client,error){
-	resp,err:=http.Get(LIB_LOGIN_URL)
-	if err!=nil{
-		return nil,utils.ERROR_SERVER
+func (this *Lib) Login(ID, Pwd string) (*http.Client, error) {
+	resp, err := http.Get(LIB_LOGIN_URL)
+	if err != nil {
+		return nil, utils.ERROR_SERVER
 	}
-	doc,err:=goquery.NewDocumentFromReader(resp.Body)
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
 	resp.Body.Close()
-	if err!=nil{
-		return nil,utils.ERROR_SERVER
+	if err != nil {
+		return nil, utils.ERROR_SERVER
 	}
-	reqData:=url.Values{
-		"txtName":{ID},
-		"txtPassWord":{Pwd},
-		"Logintype":{"RbntRecno"},
-		"BtnLogin":{"登 录"},
+	reqData := url.Values{
+		"txtName":     {ID},
+		"txtPassWord": {Pwd},
+		"Logintype":   {"RbntRecno"},
+		"BtnLogin":    {"登 录"},
 	}
-	reqData.Add("__VIEWSTATE",doc.Find("#__VIEWSTATE").AttrOr("value",""))
-	reqData.Add("__VIEWSTATEGENERATOR",doc.Find("#__VIEWSTATEGENERATOR").AttrOr("value",""))
-	reqData.Add("__EVENTVALIDATION",doc.Find("#__EVENTVALIDATION").AttrOr("value",""))
+	reqData.Add("__VIEWSTATE", doc.Find("#__VIEWSTATE").AttrOr("value", ""))
+	reqData.Add("__VIEWSTATEGENERATOR", doc.Find("#__VIEWSTATEGENERATOR").AttrOr("value", ""))
+	reqData.Add("__EVENTVALIDATION", doc.Find("#__EVENTVALIDATION").AttrOr("value", ""))
 
-	req,err:=http.NewRequest("POST",LIB_LOGIN_URL,strings.NewReader(reqData.Encode()))
-	req.Header.Add("Content-Type","application/x-www-form-urlencoded")
-	if err!=nil{
-		return nil,utils.ERROR_SERVER
+	req, err := http.NewRequest("POST", LIB_LOGIN_URL, strings.NewReader(reqData.Encode()))
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	if err != nil {
+		return nil, utils.ERROR_SERVER
 	}
-	jar,_:=cookiejar.New(nil)
-	client:=&http.Client{
-		Jar:jar,
+	jar, _ := cookiejar.New(nil)
+	client := &http.Client{
+		Jar: jar,
 	}
-	resp,err=client.Do(req)
-	if err!=nil{
-		return nil,utils.ERROR_SERVER
+	resp, err = client.Do(req)
+	if err != nil {
+		return nil, utils.ERROR_SERVER
 	}
 	defer resp.Body.Close()
-	data,_:=ioutil.ReadAll(resp.Body)
+	data, _ := ioutil.ReadAll(resp.Body)
 	//账号密码错误
-	if !strings.Contains(string(data),"图书续借"){
-		return nil,utils.ERROR_ID_PWD
+	if !strings.Contains(string(data), "图书续借") {
+		return nil, utils.ERROR_ID_PWD
 	}
 	//登录成功
-	return client,nil
+	return client, nil
 }
 
 //借阅列表
-func (this *Lib)List(ID,Pwd string)([]Book,error){
-	client,err:=this.Login(ID,Pwd)
-	if err!=nil{ //登录失败返回
-		return []Book{},err
+func (this *Lib) List(ID, Pwd string) ([]Book, error) {
+	client, err := this.Login(ID, Pwd)
+	if err != nil { //登录失败返回
+		return []Book{}, err
 	}
 	//新建请求,获取借书列表
-	req,_:=http.NewRequest("GET",LIB_BOOK_URL,nil)
+	req, _ := http.NewRequest("GET", LIB_BOOK_URL, nil)
 
-	resp,err:=client.Do(req)
-	if err!=nil{
-		return []Book{},utils.ERROR_SERVER
+	resp, err := client.Do(req)
+	if err != nil {
+		return []Book{}, utils.ERROR_SERVER
 	}
 	defer resp.Body.Close()
-	doc,err:=goquery.NewDocumentFromReader(resp.Body)
-	if err!=nil{
-		return []Book{},utils.ERROR_SERVER
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return []Book{}, utils.ERROR_SERVER
 	}
-	book:= Book{}
-	books:=[]Book{}
+	book := Book{}
+	books := []Book{}
 	doc.Find("#flexitable tbody tr").Each(func(i int, s *goquery.Selection) {
-		td:=s.Find("td")
+		td := s.Find("td")
 		book.BarCode = td.Eq(1).Text()
 		book.BookName = td.Eq(2).Text()
 		book.BookNo = td.Eq(3).Text()
@@ -97,12 +98,53 @@ func (this *Lib)List(ID,Pwd string)([]Book,error){
 		book.Price = td.Eq(8).Text()
 		book.BorrowTimes = td.Eq(9).Text()
 
-		books=append(books,book)
+		books = append(books, book)
 	})
-	return books,nil
+	return books, nil
 }
 
-//TODO 图书续借
-func (this *Lib)Borrow(ID,Pwd string,BarCodeLists []string)([]string,error){
-	return []string{},nil
+//图书续借
+func (this *Lib) Borrow(ID, Pwd string, BarCodeLists []string) ([]Book, error) {
+	//登录获取cookie
+	client, err := this.Login(ID, Pwd)
+	if err != nil {
+		return []Book{}, err
+	}
+	reqData := "?barno="
+	for _, barNo := range BarCodeLists {
+		reqData = reqData + barNo + ";"
+	}
+	req, _ := http.NewRequest("GET", LIB_REBORROW_URL+reqData, nil)
+	resp, err := client.Do(req)
+	if err != nil {
+		return []Book{}, utils.ERROR_SERVER
+	}
+	defer resp.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return []Book{}, utils.ERROR_SERVER
+	}
+	//解析页面
+	books := []Book{}
+	fmt.Println(doc.Html())
+	doc.Find("#flexitable tbody tr").Each(func(i int, s *goquery.Selection) {
+		td := s.Find("td")
+		book := Book{
+			BarCode:      td.Eq(1).Text(),
+			BookName:     td.Eq(2).Text(),
+			BookNo:       td.Eq(3).Text(),
+			BorrowedDate: td.Eq(4).Text(),
+			ReturnedDate: td.Eq(5).Text(),
+			BorrowTimes:  td.Eq(6).Text(),
+		}
+		book.ReloanRes = strings.Trim(td.Eq(0).Text(), "\n ")
+		if strings.Contains(book.ReloanRes, "续借成功,可返回查看结果") {
+			book.ReloanRes = "续借成功"
+		}
+		if strings.Contains(book.ReloanRes, "超过续借次数, 不能续借") {
+			book.ReloanRes = "超过续借次数"
+		}
+		books = append(books, book)
+	})
+	return books, nil
 }
