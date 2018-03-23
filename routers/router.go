87c35d0cc6c14a06b54ca42ce8084ae5208b212c
csuhan <1593407377@qq.com>
2018-03-24 00:57:57 +0800

    add classroom controller

--- routers/router.go
+++ routers/router.go
@@ -10,17 +10,18 @@ func init() {
 	beego.Router("/", &controllers.MainController{})
 
 	ns := beego.NewNamespace("/api",
-			beego.NSNamespace("/v1",
-				beego.NSInclude(&controllers.JwcController{}),
-				beego.NSInclude(&controllers.BusController{}),
-				beego.NSInclude(&controllers.JobController{}),
-				beego.NSInclude(&controllers.CetController{}),
-				beego.NSInclude(&controllers.LibController{}),
-			),
-		)
-	wx:=beego.NewNamespace("/wxapp",
+		beego.NSNamespace("/v1",
+			beego.NSInclude(&controllers.JwcController{}),
+			beego.NSInclude(&controllers.BusController{}),
+			beego.NSInclude(&controllers.JobController{}),
+			beego.NSInclude(&controllers.CetController{}),
+			beego.NSInclude(&controllers.LibController{}),
+			beego.NSInclude(&controllers.ClassRoomController{}),
+		),
+	)
+	wx := beego.NewNamespace("/wxapp",
 		beego.NSInclude(&controllers.WxUserController{}),
-	);
+	)
 	beego.AddNamespace(wx)
 	beego.AddNamespace(ns)
 }
